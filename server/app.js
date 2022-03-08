d92dd9125e67c49318f08d4578fa8cbbc2f4fe8c
SungGil-5125 <sunggil0125@naver.com>
2022-03-08 16:10:31 +0900

    main

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use('/user', register, login);
 app.use('/user', email);
 
+const ip = '10.120.74.59';
 app.listen(3000, function(){
     console.log("서버 실행중");
 })
